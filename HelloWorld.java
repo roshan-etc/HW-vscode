c7297dfbf90ebaac102597d963968afda236e2f0
Roshan <Roshan@gmail.com>
2024-07-10 11:01:21 +0530

    resolve

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("Hello world changes from local main from remote!");
-=======
-    System.out.println("Hello world changes from local main ok !");
->>>>>>> 690b627 (local again changes)
+    System.out.println("Hello world changes from local main ok");
   }
 }
